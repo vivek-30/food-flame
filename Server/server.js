44c515035da2191648923f1f6f577e80671d0b0b
vivek singh <vikky30singh@gmail.com>
2023-02-02 01:21:06 +0530

    connection to database established and ensured that all recipe routes are up and running...

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const http = require('http');
+require('dotenv').config();
+
+const recipesRouter = require('./Router/recipesRouter');
+const usersRouter = require('./Router/recipesRouter');
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
+const DATABASE_URI = process.env.DATABASE_URI;
 
-server.listen(PORT, () => {
-  console.log(`Server is Running at PORT: ${PORT}`);
-});
+app.use(express.json());
+app.use(recipesRouter);
+app.use(usersRouter);
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(DATABASE_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is Running at PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
