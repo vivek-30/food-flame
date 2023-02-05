eea9f2167aea878800c8976212e358e81298d6be
vivek singh <vikky30singh@gmail.com>
2023-02-05 16:24:50 +0530

    injected built components and pages in app.js file and added routing support with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Components.
+import Navbar from './components/Navbar';
+import Authentication from './components/Authentication';
+
+// Pages.
+import Home from './pages/Home';
+import Landing from './pages/Landing';
+
 const App = () => {
   return (
     <div className="App">
-      Recipes App
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing /> } />
+          <Route path="/home" element={<Home />} />
+          <Route path="/sign-up" element={<Authentication title="Sign Up" />} />
+          <Route path="/log-in" element={<Authentication title="Log In" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
