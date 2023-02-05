20ec2e3eb7bbdde4839f4150c8d7d8fe0c1387c2
vivek singh <vikky30singh@gmail.com>
2023-02-05 16:41:11 +0530

    added CORS support in backend

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
+const cors = require('cors');
 require('dotenv').config();
 
 const recipesRouter = require('./Router/recipesRouter');
@@ -10,10 +11,16 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
 const DATABASE_URI = process.env.DATABASE_URI;
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE']
+};
 
 app.use(express.json());
-app.use(recipesRouter);
-app.use(usersRouter);
+app.use(cors(corsOptions));
+
+app.use('/user', usersRouter);
+app.use('/recipes', recipesRouter);
 
 mongoose.set('strictQuery', false);
 
