87c1210e8ad8fbc933c5410006e57aab10ac9047
vivek singh <vikky30singh@gmail.com>
2023-02-05 20:44:55 +0530

    created recipe component

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,12 +1,24 @@
 const Recipe = ({ recipeObject }) => {
-  const { name, description, ingredients, cookingSteps, imageSRC } = recipeObject;
+  const { name, description, imageSRC } = recipeObject;
   return (
-    <div className="recipe-card">
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>{ingredients}</p>
-      <strong>{imageSRC}</strong>
-      <p>{cookingSteps}</p>
+    <div className="card recipe-card col s12 m5 l3">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src={imageSRC} alt={name} />
+      </div>
+      <div class="card-content">
+        <strong class="card-title activator grey-text text-darken-4 left">
+          {name}
+          <i class="material-icons">more_vert</i>
+        </strong>
+        {/* <p><a href="#">This is a link</a></p> */}
+      </div>
+      <div class="card-reveal grey lighten-4">
+        <span class="card-title grey-text text-darken-4">
+          {name}
+          <i class="material-icons right">close</i>
+        </span>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
