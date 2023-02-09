88a523b34caa0b5f97b8a0c7ac86760de79f5848
vivek singh <vikky30singh@gmail.com>
2023-02-10 01:13:35 +0530

    created RecipeUpdateBox component

---
+++ client/src/components/RecipeUpdateBox.js
@@ -0,0 +1,51 @@
+import { useNavigate } from 'react-router-dom';
+
+const RecipeUpdateBox = ({ recipeID }) => {
+
+  const navigate = useNavigate();
+
+  const navigateBackward = () => {
+    navigate('/home');
+  }
+
+  const removeRecipe = async () => {
+    const response = await fetch(`http://localhost:4000/recipes/${recipeID}`, { method: 'DELETE' });
+    const data = await response.json();
+
+    if(response.ok) {
+      navigate('/home');
+    } else {
+      const { message, error } = data;
+      alert(message);
+      console.log(`Error Occured While Deleting A Recipes ${error}`);
+    }
+  }
+
+  const updateRecipe = () => {
+    console.log('updating....')
+  }
+
+  return (
+    <div className="col s10 m10 l10" id="recipe-update-box">
+      <div className="col s6 m5 l4">
+        <i 
+          onClick={navigateBackward}
+          className="material-icons small blue-grey-text text-darken-2 z-depth-1">
+          arrow_back
+        </i>
+        <i 
+          onClick={removeRecipe}
+          className="material-icons small blue-grey-text text-darken-2 z-depth-1">
+          delete
+        </i>
+        <i 
+          onClick={updateRecipe}
+          className="material-icons small blue-grey-text text-darken-2 z-depth-1">
+          edit
+        </i>
+      </div>
+    </div>
+  );
+}
+
+export default RecipeUpdateBox;
