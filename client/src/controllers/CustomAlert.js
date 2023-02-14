bb5f8c5d4adf955abbe49f2733aa3b1f8557a253
vivek singh <vikky30singh@gmail.com>
2023-02-14 20:36:34 +0530

    created CustomAlert Controller

---
+++ client/src/controllers/CustomAlert.js
@@ -0,0 +1,13 @@
+import M from 'materialize-css';
+
+const customAlert = (message) => {
+  M.toast({
+    html: message,
+    inDuration: 800,
+    outDuration: 800,
+    displayLength: 5000,
+    classes: 'rounded grey lighten-5 blue-grey-text text-darken-4'
+  });
+}
+
+export default customAlert;
