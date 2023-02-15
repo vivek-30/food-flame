9594a6fbeb7a20764ce50a716eeb2364a8f02508
vivek singh <vikky30singh@gmail.com>
2023-02-15 17:22:11 +0530

    styled navbar and enabled page scrolling even when sidenav is open

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,29 +7,31 @@ const Navbar = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      M.Sidenav.init(sideNavRef.current);
+      M.Sidenav.init(sideNavRef.current, {
+        preventScrolling: false
+      });
     }, 1000);
   },  []);
   
   return (
     <>
       <nav className="blue-grey white">
-        <span className="sidenav-trigger white valign-wrapper" data-target="slide-out">
-          <i className="material-icons black-text">menu</i>
+        <span className="sidenav-trigger white" data-target="slide-out">
+          <i className="material-icons blue-grey-text text-darken-2">menu</i>
         </span>
         <div className="nav-wrapper container">
-          <Link to="/" className="brand-logo black-text">
-            <i className="material-icons hide-on-small-only">local_dining</i>
+          <Link to="/" className="brand-logo blue-grey-text text-darken-2">
+            <i className="material-icons blue-grey-text text-darken-2 hide-on-small-only">local_dining</i>
             FoodFlame
           </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/sign-up" className="blue-grey-text text-darken-1">Sign Up</Link></li>
-            <li><Link to="/log-in" className="blue-grey-text text-darken-1">Log In</Link></li>
+            <li><Link to="/sign-up" className="blue-grey-text text-darken-2">Sign Up</Link></li>
+            <li><Link to="/log-in" className="blue-grey-text text-darken-2">Log In</Link></li>
           </ul>
         </div>
       </nav>
       <ul ref={sideNavRef} id="slide-out" className="sidenav blue-grey lighten-5">
-        <li><div className="user-view flow-text">FoodFlame</div></li>
+        <li><div className="user-view flow-text blue-grey-text text-darken-2">FoodFlame</div></li>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/sign-up">Sign Up</Link></li>
         <li><Link to="/log-in">Log In</Link></li>
