b51db34cd77f4195b472d963528d54ef6d5e7dda
vivek singh <vikky30singh@gmail.com>
2023-02-19 01:11:21 +0530

    completed authControllers for both signup and login along with cookie setup

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -1,12 +1,44 @@
-const handleUserSignUp = async (req, res) => {
+const Users = require('../Models/userModel');
 
+const maxExpireTime = 3 * 24 * 60 * 60 * 1000;   // 3 Days.
+
+const handleUserSignUp = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const { user, token } = await Users.signup(username, email, password);
+
+    // Set token in user's cookie.
+    res.cookie('jwt', token, {
+      maxAge: maxExpireTime,  
+      httpOnly: true,
+      secure: true
+    });
+    
+    res.status(200).json(user);
+  } catch(error) {
+    res.status(401).json({ error: error.message });
+  }
 }
 
-const handleUserSignIn = async (req, res) => {
+const handleUserLogIn = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const { user, token } = await Users.login(email, password);
 
+    // Set token in user's cookie.
+    res.cookie('jwt', token, {
+      maxAge: maxExpireTime,
+      httpOnly: true,
+      secure: true
+    });
+
+    res.status(200).json(user);
+  } catch(error) {
+    res.status(401).json({ error: error.message });
+  }
 }
 
 module.exports = {
   handleUserSignUp,
-  handleUserSignIn
+  handleUserLogIn
 };
