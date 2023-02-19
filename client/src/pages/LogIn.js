6baa7c0e67567bff9b297648a3cbe61011190f77
vivek singh <vikky30singh@gmail.com>
2023-02-19 22:01:03 +0530

    completed login functionality

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,21 +1,62 @@
+import { useState } from 'react';
+import useLogin from '../hooks/useLogin';
+import customAlert from '../utils/customAlert';
+
+const emptyCredentials = {
+  email: '',
+  password: ''
+};
 const LogIn = () => {
+  const [credentials, setCredentials] = useState(emptyCredentials);
+  const { loginUser, isLoading, error } = useLogin();
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((currentCredentials) => ({ ...currentCredentials, [id]: value }));
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    await loginUser(credentials);
+    console.log(error)
+    if(error) {
+      customAlert(error);
+    } else {
+      setCredentials(emptyCredentials);
+    }
+  }
+
   return (
-    <div className="container authentication">
+    <div className="container auth-container">
       <div className="container">
         <h4 className="center-align">Log In</h4>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="input-field">
-            <i className="material-icons prefix">account_circle</i>
-            <input id="username" type="text" className="validate" />
-            <label htmlFor="username">Enter Your UserName</label>
+            <i className="material-icons prefix">email</i>
+            <input
+              id="email"
+              type="email"
+              autoComplete="off"
+              spellCheck="false"
+              value={credentials.email}
+              onChange={handleInputChange}
+            />
+            <label htmlFor="email">Enter Your Email</label>
           </div>
           <div className="input-field">
             <i className="material-icons prefix">vpn_key</i>
-            <input id="password" type="password" className="validate" />
+            <input
+              id="password"
+              type="password"
+              autoComplete="off"
+              spellCheck="false"
+              value={credentials.password}
+              onChange={handleInputChange}
+            />
             <label htmlFor="password">Enter Your Password</label>
           </div>
           <div className="center-align">
-            <button className="btn waves-effect waves-light darken-2" type="submit" name="log-in">
+            <button disabled={isLoading} className="btn teal darken-1" type="submit">
               Log In
               <i className="material-icons right">send</i>
             </button>
