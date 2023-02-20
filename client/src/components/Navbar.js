3de842a3f1d18d1ee3812bbe43e8504e771db5ec
vivek singh <vikky30singh@gmail.com>
2023-02-20 20:23:52 +0530

    removed "sidenav" and added "FoodFlame" logo in navbar.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,13 @@
-import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import M from 'materialize-css';
-
 import useLogout from '../hooks/useLogout';
-import customAlert from '../utils/customAlert';
 import useAuthContext from '../hooks/useAuthContext';
+
+import customAlert from '../utils/customAlert';
+import AppLogo from '../assets/FoodFlame-logo.png';
 
 const Navbar = () => {
   const { state } = useAuthContext();
   const { logoutUser, isLoading, error } = useLogout();
-
-  const sideNavRef = useRef();
-
-  useEffect(() => {
-    setTimeout(() => {
-      M.Sidenav.init(sideNavRef.current, {
-        preventScrolling: false
-      });
-    }, 1000);
-  },  []);
 
   const handleUserLogout = async () => {
     await logoutUser();
@@ -28,42 +17,31 @@ const Navbar = () => {
   }
   
   return (
-    <>
-      <nav className="blue-grey white">
-        <span className="sidenav-trigger white cursor-ptr" data-target="slide-out">
-          <i className="material-icons blue-grey-text text-darken-2">menu</i>
-        </span>
-        <div className="nav-wrapper container">
-          <Link to="/" className="brand-logo blue-grey-text text-darken-2">
-            <i className="material-icons blue-grey-text text-darken-2 hide-on-small-only">local_dining</i>
-            FoodFlame
-          </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            {
-              state.user ? 
-                <li>
-                  <button 
-                    disabled={isLoading}
-                    onClick={handleUserLogout}
-                    className="auth-btn cursor-ptr blue-grey-text text-darken-2">
-                      Log Out
-                  </button>
-                </li> :
-                <>
-                  <li><Link to="/sign-up" className="blue-grey-text text-darken-2">Sign Up</Link></li>
-                  <li><Link to="/log-in" className="blue-grey-text text-darken-2">Log In</Link></li>
-                </>
-            }
-          </ul>
-        </div>
-      </nav>
-      <ul ref={sideNavRef} id="slide-out" className="sidenav blue-grey lighten-5">
-        <li><div className="user-view flow-text blue-grey-text text-darken-2">FoodFlame</div></li>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/sign-up">Sign Up</Link></li>
-        <li><Link to="/log-in">Log In</Link></li>
-      </ul>
-    </>
+    <nav className="blue-grey white">
+      <div className="nav-wrapper container">
+        <img src={AppLogo} alt="Logo" id="app-logo" />
+        <Link to="/" className="brand-logo hide-on-small-only blue-grey-text text-darken-2">
+          FoodFlame
+        </Link>
+        <ul id="nav-mobile" className="right">
+          {
+            state.user ? 
+              <li>
+                <button 
+                  disabled={isLoading}
+                  onClick={handleUserLogout}
+                  className="auth-btn cursor-ptr blue-grey-text text-darken-2">
+                    Log Out
+                </button>
+              </li> :
+              <>
+                <li><Link to="/sign-up" className="blue-grey-text text-darken-2">Sign Up</Link></li>
+                <li><Link to="/log-in" className="blue-grey-text text-darken-2">Log In</Link></li>
+              </>
+          }
+        </ul>
+      </div>
+    </nav>
   );
 }
 
