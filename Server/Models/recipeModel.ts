1cbd4c5a44da825c9591d7d37f2ef4652ec6330a
vivek singh <vikky30singh@gmail.com>
2023-06-02 14:51:45 +0530

    refactor: migrated recipeModel.js to recipeModel.ts

--- Server/Models/recipeModel.ts
+++ Server/Models/recipeModel.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const recipeSchema = new mongoose.Schema({
   name: {
@@ -27,4 +27,4 @@ const recipeSchema = new mongoose.Schema({
   }
 }, {timestamps: true});
 
-module.exports = mongoose.model('Recipe', recipeSchema);
+export default mongoose.model('Recipe', recipeSchema);
