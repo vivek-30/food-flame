d5f6f7e25b6fb12318c48c46fdfd04e212616c0f
vivek singh <vikky30singh@gmail.com>
2023-06-10 20:53:46 +0530

    refactor: migrated useRecipeContext.js to useRecipeContext.ts

--- client/src/hooks/useRecipeContext.ts
+++ client/src/hooks/useRecipeContext.ts
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
 import { RecipeContext } from '../context/RecipeContext';
+import { IRecipeContext } from '../types/index.interfaces';
 
-const useRecipeContext = () => {
-  const context = useContext(RecipeContext);
+const useRecipeContext = (): IRecipeContext => {
+  const context = useContext<IRecipeContext | null>(RecipeContext);
   const contextScopeError = new Error('useRecipeContext Hook Must Be Used WithIn RecipeContext.Provider Component.');
 
   if(context === null) {
