bbe9dda930d2de97e60943e2bcf257eb652f5732
vivek singh <vikky30singh@gmail.com>
2023-06-10 20:42:56 +0530

    refactor: migrated RecipeContext.js to RecipeContext.tsx

--- client/src/context/RecipeContext.tsx
+++ client/src/context/RecipeContext.tsx
@@ -1,15 +1,16 @@
 import { createContext, useReducer } from 'react';
 
-// Reducers.
 import recipeReducer from '../reducers/recipeReducer';
+import { RecipeContextState } from '../types/index.types';
+import { IRecipeContext, IRecipeContextProp } from '../types/index.interfaces';
 
 // Context and state.
-export const RecipeContext = createContext(null);
-const initialState = {
+export const RecipeContext = createContext<IRecipeContext | null>(null);
+const initialState: RecipeContextState = {
   recipes: []
 };
 
-const RecipeContextProvider = ({ children }) => {
+const RecipeContextProvider = ({ children }: IRecipeContextProp) => {
   const [state, dispatch] = useReducer(recipeReducer, initialState);
 
   return (
