1c1565ce0e1ab541fc9240d43d950d0ceb77fea3
vivek singh <vikky30singh@gmail.com>
2023-06-10 21:03:04 +0530

    refactor: migrated EmailVerification.js to EmailVerification.tsx

--- client/src/pages/EmailVerification.tsx
+++ client/src/pages/EmailVerification.tsx
@@ -1,32 +1,34 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 import useAuthContext from '../hooks/useAuthContext';
 
-import { VERIFY_EMAIL_URI } from '../utils/URIs';
 import AppLogo from '../assets/FoodFlame-logo.png';
+import { VERIFY_EMAIL_URI } from '../../constants/URIs';
+import { LogInResponseData } from '../types/index.types';
 
 import LoadingSpinner from '../components/LoadingSpinner';
 
 const EmailVerification = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   
   const [searchParams] = useSearchParams();
   const { dispatch } = useAuthContext();
 
-  const _token = searchParams.get('_token');
+  const _token: string | null = searchParams.get('_token');
 
   useEffect(() => {
     (async () => {
       setIsLoading(true);
       const response = await fetch(`${VERIFY_EMAIL_URI}?_token=${_token}`, { credentials: 'include' });
-      const data = await response.json();
+      const data: LogInResponseData = await response.json();
       setIsLoading(false);
-      if(response.ok) {
-        dispatch({ type: 'LOGIN', payload:data._id });
+      if(response.ok && !data.error) {
+        dispatch({ type: 'LOGIN', payload: data._id });
         localStorage.setItem('user', JSON.stringify(data._id));
       } else {
-        setError(data.error);
+        const errorMessage = data.error || 'Unknown error occured during email verification process.';
+        setError(errorMessage);
       }
       
     })();
