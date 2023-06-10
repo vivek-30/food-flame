5225c6e9be430536adcb57f69636279be009357c
vivek singh <vikky30singh@gmail.com>
2023-06-10 20:46:28 +0530

    refactor: migrated useSignup.js to useSignup.ts

---
+++ client/src/hooks/useSignup.ts
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { SIGNUP_URI } from '../../constants/URIs';
+
+import { SignUpResponseData } from '../types/index.types';
+import { ISignUpCredentials } from '../types/index.interfaces';
+
+const useSignup = () => {
+  const [info, setInfo] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signupUser = async (signupCredentials: ISignUpCredentials): Promise<void> => {
+    setIsLoading(true);
+    setError(null);
+    setInfo(null);
+    const response = await fetch(SIGNUP_URI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(signupCredentials),
+      credentials: 'include'
+    });
+    const data: SignUpResponseData = await response.json();
+
+    setIsLoading(false);
+    if(response.ok && data.message) {
+      setInfo(data.message);
+    } else {
+      const errorMessage = data.error || 'Unknown error occured during signup process.';
+      setError(errorMessage);
+    }
+  }
+
+  return { signupUser, isLoading, info, error };
+}
+
+export default useSignup;
